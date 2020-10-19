9fae149dca98139b87d45cdfe45b52aef25e14ae
KroFin <39234331+SteveMiller233@users.noreply.github.com>
2020-10-19 15:31:07 +0800

    Update shell script

--- shell/restart.sh
+++ shell/restart.sh
@@ -15,3 +15,15 @@ kill -9 $tpid
 else
 echo 'Stop Success!'
 fi
+
+tpid=`ps -ef|grep $RESOURCE_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+if [ ${tpid} ]; then
+    echo 'App is running.'
+else
+    echo 'App is NOT running.'
+fi
+
+rm -f tpid
+nohup java -jar ./$RESOURCE_NAME --spring.profiles.active=test &
+echo $! > tpid
+echo Start Success!
--- shell/start.sh
+++ shell/start.sh
@@ -1,20 +1,6 @@
 #!/bin/sh
+
 RESOURCE_NAME=demo-0.0.1-SNAPSHOT.jar
-
-tpid=`ps -ef|grep $RESOURCE_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
-if [ ${tpid} ]; then
-echo 'Stop Process...'
-kill -15 $tpid
-fi
-sleep 5
-
-tpid=`ps -ef|grep $RESOURCE_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
-if [ ${tpid} ]; then
-echo 'Kill Process!'
-kill -9 $tpid
-else
-echo 'Stop Success!'
-fi
 
 tpid=`ps -ef|grep $RESOURCE_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
 if [ ${tpid} ]; then
